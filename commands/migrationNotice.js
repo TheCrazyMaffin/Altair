2a3c004a3daffd715bf47e105b5048b202714349
TheCrazyMaffin <thecrazymaffin@gmail.com>
2021-02-17 18:50:44 +0100

    Fix a crash when a guild doesnt have the altair channel

--- commands/migrationNotice.js
+++ commands/migrationNotice.js
@@ -6,7 +6,12 @@ module.exports = class {
   async run(message) {
     if (message.staff) {
       for (const guild of global.Client.guilds.cache.array()) {
-        guild.channels.cache.find((ch) => ch.name.includes('altair')).send('We are performing some updates to improve altair. Your autoban settings have been saved but you may set a custom logging channel now. If youd wish to keep the current channel please execute `+logging #altair`. To find out more contact us on the support server or type `+help`');
+        const altairChan = guild.channels.cache.find((ch) => ch.name.includes('altair'));
+        if (typeof altairChan !== 'undefined') {
+          altairChan.send('We are performing some updates to improve altair. Your autoban settings have been saved but you may set a custom logging channel now. If youd wish to keep the current channel please execute `+logging #altair`. To find out more contact us on the support server or type `+help`');
+        } else {
+          message.channel.send(`Could not notify ${guild.name}. They do not have a #altair channel`);
+        }
       }
       message.react('✅');
     } else {
