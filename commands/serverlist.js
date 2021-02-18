121fd920942fb063effa4c67f6a77cc5ab7b84df
TheCrazyMaffin <thecrazymaffin@gmail.com>
2021-02-18 13:26:57 +0100

    Added a serverlist command

---
+++ commands/serverlist.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const AltairGuilds = require('../src/js/guilds');
+
+module.exports = class {
+  constructor() {
+    this.name = 'serverlist';
+  }
+
+  /**
+   *
+   * @param {Discord.Message} message
+   */
+  async run(message) {
+    if (message.staff) {
+      const query = message.content.split(' ')[1];
+      const guilds = await (new AltairGuilds()).fetchGuilds();
+      let txt = '**Name | Autobans | Logging**\n';
+      for (const guild of guilds.guilds) {
+        if (!guild.available) return;
+        txt += `${guild.name} | ${guild.autobans === 1 ? '✅' : '❌'} | ${guild.logging === null ? 'Disabled' : `<#${guild.logging.id}>`}\n`;
+      }
+      message.channel.send(txt, { split: true });
+    } else {
+      message.channel.send('🔐 You cannot use this command');
+    }
+  }
+};
