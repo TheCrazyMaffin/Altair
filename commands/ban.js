12ed32204c56844e1eb68525d6c7a54b819853e0
TheCrazyMaffin <thecrazymaffin@gmail.com>
2021-03-08 16:07:17 +0100

    This repo has a eslint config for a reason.

--- commands/ban.js
+++ commands/ban.js
@@ -1,18 +1,18 @@
 const AltairBan = require('../src/js/ban');
 
 module.exports = class {
-    constructor() {
-        this.name = 'ban';
-    }
+  constructor() {
+    this.name = 'ban';
+  }
 
-    async run(message) {
-        if (message.staff) {
-            const ban = new AltairBan(message.content.split(' ')[1]);
-            await ban.ban();
-            await ban.update(message.content.split(' ').slice(2).join(' '));
-            message.react('✅');
-        } else {
-            message.channel.send('🔐 You cannot use this command');
-        }
+  async run(message) {
+    if (message.staff) {
+      const ban = new AltairBan(message.content.split(' ')[1]);
+      await ban.ban();
+      await ban.update(message.content.split(' ').slice(2).join(' '));
+      message.react('✅');
+    } else {
+      message.channel.send('🔐 You cannot use this command');
     }
+  }
 };
